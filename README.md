f36b2f27ee9661cefaf9a82a43d9b2afd67ee07f
Graham Voysey <gvoysey@bu.edu>
2014-02-21 16:21:10 -0500

    updated README

---
+++ README.md
@@ -0,0 +1,4 @@
+GAWA-Website
+============
+
+Coursework/ Final Project that created a website for displaying images taken to determine the mechanical properties of the basilar membrane in gerbils.
--- cgi-bin/gawa.py
+++ cgi-bin/gawa.py
@@ -1,5 +1,6 @@
 #!/usr/local/Python-2.7.3/bin/python
 import sys, MySQLdb, cgi, cgitb, re, random, string, datetime, os, os.path, json, datetime
+
 cgitb.enable()
 
 ####  THIS IS OUR FINAL PROJECT CODE ####
@@ -9,89 +10,93 @@ cgitb.enable()
 
 ### DATABASE CONNECTION CODE ###
 
-def connect_db(username,pswd):
-	#connect to database
-	connection = MySQLdb.connect(host="localhost", db="gawa", user=username, passwd=pswd)
-	#get cursor
-	cursor = connection.cursor()
-	return cursor, connection
+def connect_db(username, pswd):
+    #connect to database
+    connection = MySQLdb.connect(host="localhost", db="gawa", user=username, passwd=pswd)
+    #get cursor
+    cursor = connection.cursor()
+    return cursor, connection
+
 
 def close_db(cursor, connection):
-	#close cursor and connection
-	cursor.close()
-	connection.close()
+    #close cursor and connection
+    cursor.close()
+    connection.close()
 
 
 ### THESE DEFINITIONS ARE FROM GawaImage.py FROM AMAUCHE E. 5/2/13 2:30 PM ###
 
 def htmlImageFolder():
-        return '/www/html/students_13/gawa/'
+    return '/www/html/students_13/gawa/'
+
 
 def image_timeout():
-	return 24*60*60*100000 #24 hours in deciseconds
+    return 24 * 60 * 60 * 100000 #24 hours in deciseconds
+
 
 def zip_timeout():
-	return 24*60*60*100000 #24 hours in deciseconds
+    return 24 * 60 * 60 * 100000 #24 hours in deciseconds
+
 
 def createAnimatedGIF(strArr):
-	if (not isinstance(strArr,list)):
-		print "Input is not an array"
-		return None
-	if len(strArr)<1:
-		print "Array cannot be empty"
-		return []
+    if (not isinstance(strArr, list)):
+        print "Input is not an array"
+        return None
+    if len(strArr) < 1:
+        print "Array cannot be empty"
+        return []
 
-	from PIL import Image
-	from images2gif import writeGif
+    from PIL import Image
+    from images2gif import writeGif
 
-	resultArr = convertToGIF(strArr,700,fullPath=False)
-	images = [Image.open(fn) for fn in resultArr]
-	tempfolder = os.path.split(resultArr[0])[0]
-	epochtime = str(datetime.datetime.now().strftime('%s%f'))
-	dstPath = os.path.join(tempfolder,"my_gif"+epochtime+".GIF")
-	output = writeGif(dstPath,images, duration=0.2)
+    resultArr = convertToGIF(strArr, 700, fullPath=False)
+    images = [Image.open(fn) for fn in resultArr]
+    tempfolder = os.path.split(resultArr[0])[0]
+    epochtime = str(datetime.datetime.now().strftime('%s%f'))
+    dstPath = os.path.join(tempfolder, "my_gif" + epochtime + ".GIF")
+    output = writeGif(dstPath, images, duration=0.2)
 
-	if(os.path.exists(dstPath)):
-		return [getURLPath(dstPath)]
-	else:
-		print "I failed to create animated GIF "+filename
-		return None
-
-def convertToGIF(strArr,size=-1,fullPath=True):
-	if (not isinstance(strArr,list)):
-		print "Input is not an array"
-		return None
-	if len(strArr)<1:
-		print "Array cannot be empty"
-		return []
-
-	import shutil
-	from subprocess import call
-	
-	epochtime = str(datetime.datetime.now().strftime('%s%f'))
-	tempfolder = os.path.join(htmlImageFolder(),"anigif",epochtime) 
-
-	if not os.path.exists(tempfolder):os.makedirs(tempfolder)
-	resultArr = []
-	
-	#This section removes all th directories created that are older than 6 minutes.
-	for top, dirs, files in os.walk(os.path.dirname(tempfolder)):
-	    for subd in dirs:
-		if subd.isdigit(): 
-			tstamp = int(subd)
-			if int(datetime.datetime.now().strftime('%s%f'))-tstamp>image_timeout():
-                                try :
-                                        shutil.rmtree(os.path.join(top,subd))
-                                except :
-                                        pass
+    if (os.path.exists(dstPath)):
+        return [getURLPath(dstPath)]
+    else:
+        print "I failed to create animated GIF " + filename
+        return None
 
 
-	for fiPath in strArr:
-		if fiPath.endswith('.tif'):
-			
-			justName = os.path.split(fiPath)[-1].split('.')[0]
-			dstPath = os.path.join(tempfolder,justName + ".gif")
-			if os.path.exists(dstPath):os.remove(dstPath)			
+def convertToGIF(strArr, size=-1, fullPath=True):
+    if (not isinstance(strArr, list)):
+        print "Input is not an array"
+        return None
+    if len(strArr) < 1:
+        print "Array cannot be empty"
+        return []
+
+    import shutil
+    from subprocess import call
+
+    epochtime = str(datetime.datetime.now().strftime('%s%f'))
+    tempfolder = os.path.join(htmlImageFolder(), "anigif", epochtime)
+
+    if not os.path.exists(tempfolder): os.makedirs(tempfolder)
+    resultArr = []
+
+    #This section removes all th directories created that are older than 6 minutes.
+    for top, dirs, files in os.walk(os.path.dirname(tempfolder)):
+        for subd in dirs:
+            if subd.isdigit():
+                tstamp = int(subd)
+                if int(datetime.datetime.now().strftime('%s%f')) - tstamp > image_timeout():
+                    try:
+                        shutil.rmtree(os.path.join(top, subd))
+                    except:
+                        pass
+
+    for fiPath in strArr:
+        if fiPath.endswith('.tif'):
+
+            justName = os.path.split(fiPath)[-1].split('.')[0]
+            dstPath = os.path.join(tempfolder, justName + ".gif")
+            if os.path.exists(dstPath): os.remove(dstPath)
 			if(size is None or not isinstance(size,int) or size < 0):
 				output = call(["/usr/bin/convert",fiPath, dstPath])
 			else:
